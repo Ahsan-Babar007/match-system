5b93a5c7c74453917dacbaad98d52c6b058bbb83
Ahsan-Babar007 <ranaahsanbabar@gmail.com>
2025-02-28 17:02:45 +0500

    update

--- resources/views/cricket-scoreboard.blade.php
+++ resources/views/cricket-scoreboard.blade.php
@@ -334,7 +334,7 @@
                 $(".team-box:last .score").text(data.score2 || "0/0");
 
                 // Update Overs & Run Rates
-                $(".overs h3").text("Overs: " + (data.score || "0"));
+                $(".overs h3").text (data.score || "0");
                 $(".run-rate:first").text("CRR: " + (data.crr || "0.00"));
                 $(".run-rate:last").text("RRR: " + (data.rrr || "0.00"));
 
