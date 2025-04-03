14ad7344ac33a0e06e55b73340a95de1327e2327
Ahsan-Babar007 <143760080+Ahsan-Babar007@users.noreply.github.com>
2025-04-04 04:43:21 +0500

    update

--- resources/views/cricket-scoreboard.blade.php
+++ resources/views/cricket-scoreboard.blade.php
@@ -41,7 +41,7 @@
             top: 0;
             left: 0;
             width: 100%;
-            height: 120%;
+            height: 130%;
             object-fit: cover;
             z-index: -1; /* Ensure video stays behind the content */
         }
